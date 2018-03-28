fdae717ade419d765659e3df733cc68aef82a134
manuel.zavaleta <manuel.zavaleta@globant.com>
2018-03-28 17:14:05 -0600

    Fix Test StarbucksMenuTest Gradle command line gradlew test run tests add testng.xml

--- src/test/java/com/starbucks/test/StarbucksMenuTest.java
+++ src/test/java/com/starbucks/test/StarbucksMenuTest.java
@@ -8,7 +8,7 @@ import org.testng.annotations.Test;
 @Test
 public class StarbucksMenuTest extends StarbucksTest {
     public void scenario() {
-        String expectedMenuElements = "COFFE,TEA,MENU,COFFEEHOUSE,SOCIAL IMPACT,STARBUCKS REWARDS,BLOG,GIFT CARDS";
+        String expectedMenuElements = "COFFEE,TEA,MENU,COFFEEHOUSE,SOCIAL IMPACT,STARBUCKS REWARDS,BLOG,GIFT CARDS";
         String actualMenuElements = StarbucksHome.go().findMenuElementsToString();
 
         Assert.assertEquals(actualMenuElements, expectedMenuElements);
