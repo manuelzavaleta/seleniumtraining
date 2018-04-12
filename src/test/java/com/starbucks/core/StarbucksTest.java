48b0b1001932f475eefe37701f6988d51b621039
Manuel Zavaleta <manuelzm@gmail.com>
2018-04-11 22:38:11 -0500

    linux support

--- src/test/java/com/starbucks/core/StarbucksTest.java
+++ src/test/java/com/starbucks/core/StarbucksTest.java
@@ -8,7 +8,9 @@ public class StarbucksTest {
 
     @BeforeSuite(alwaysRun = true)
     public void beforeSuite() {
-        System.setProperty("webdriver.gecko.driver", "src/test/resources/geckodriver.exe");
+        String extension = System.getProperty("os.name").equals("Linux") ? "" : ".exe";
+        String driverPath = String.format("src/test/resources/geckodriver%s", extension);
+        System.setProperty("webdriver.gecko.driver", driverPath);
         System.setProperty(FirefoxDriver.SystemProperty.DRIVER_USE_MARIONETTE, "true");
         System.setProperty(FirefoxDriver.SystemProperty.BROWSER_LOGFILE, "consoleLogs_Firefox.log");
 
